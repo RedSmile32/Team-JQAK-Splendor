a34a252ff0747a8018107c068fad210ddd0c8496
Kevin21140 <k2114037@students.katyisd.org>
2024-11-07 11:21:33 -0600

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+public class Player{
+    private HashMap<Type, ArrayList<Card>> cards= new HashMap<Type, ArrayList<Card>>;
+    private HashMap<Type, Integer> tokens = new HashMap<Type, Integer>;
+    private ArrayList<Patron> patrons; 
+    private boolean p1;
+
+    public Player(boolean x){
+        p1 = x;
+    }
+    public boolean isP1(){
+        return p1;
+
+    }
+    public boolean buyCard(Card){
+        
+
+
+    }
+    public void addToken(Type)
+
+
+}
